7d41b675d50a1b04e39c4e7636b85ead956b5a14
Danny Wakeling <wakeling101@hotmail.com>
2019-03-03 19:40:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
-def reverse_each_word(string)
+def reverse_each_word_with_each(string)
   make_array = string.split(" ")
-  reversed = make_array.map(&:reverse!)
-  return reversed.join(" ")
+  new_array = []
+  make_array.each do|string|
+  new_array << string.reverse
+  return new_array.join(" ")
 end
